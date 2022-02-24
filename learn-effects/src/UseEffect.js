2278e7f43c53fe4f36ca896b3b7f91fb16a2872c
root <cgc940405@gmail.com>
2022-02-25 05:03:14 +0900

    learn 2nd arg of Effect

--- learn-effects/src/UseEffect.js
+++ learn-effects/src/UseEffect.js
@@ -2,17 +2,38 @@ import { useEffect, useState } from 'react';
 
 function UseEffect() {
     const [counter, setCounter] = useState(0);
+    const [keyword, setKeyword] = useState('');
     const onClick = () => {
         setCounter((prev) => prev + 1);
     };
-    console.log('I run all the time');
+    const onChange = (e) => {
+        setKeyword(e.target.value);
+    };
 
     useEffect(() => {
-        console.log('call the API');
+        console.log('I run only once');
     }, []);
+
+    useEffect(() => {
+        console.log("I run when 'keyword' changes");
+    }, [keyword]);
+
+    useEffect(() => {
+        console.log("I run when 'counter' changes");
+    }, [counter]);
+
+    useEffect(() => {
+        console.log("I run when 'keyword & counter' changes");
+    }, [keyword, counter]);
 
     return (
         <div>
+            <input
+                value={keyword}
+                onChange={onChange}
+                type="text"
+                placeholder="Search here..."
+            />
             <h1>{counter}</h1>
             <button onClick={onClick}>click me</button>
         </div>
